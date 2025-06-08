6783fd5e0f4ef72eadb493008865b797b7ea9bfa
yanovslo2 <yanovslo2@gmail.com>
2025-06-08 14:26:59 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,13 @@
+﻿function scrollToSection(id) {
+  const section = document.getElementById(id);
+  if (section) {
+    section.scrollIntoView({ behavior: "smooth" });
+  }
+}
+
+document.getElementById("order-form").addEventListener("submit", function (e) {
+  e.preventDefault();
+  alert("Thank you! Your preorder has been received.");
+  this.reset();
+});
+
